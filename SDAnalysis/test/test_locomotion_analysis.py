16f3e209a56002a6201ff9c51c09338b5dfab6f8
Bence Mitlasoczki <mitlabence111@gmail.com>
2024-11-01 16:13:02 +0100

    Added output comparison test for ChR2 dataset

--- SDAnalysis/test/test_locomotion_analysis.py
+++ SDAnalysis/test/test_locomotion_analysis.py
@@ -2,11 +2,14 @@ import pytest
 import pandas as pd
 import os
 import sys
+from utils.dataframe_comparison import dataframes_equal
+
 try:
     root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     sys.path.insert(0, root_dir)
 finally:
     from env_reader import read_env
+    from locomotion_analysis import main
 
 
 @pytest.fixture(scope="module")
@@ -35,15 +38,21 @@ def loco_aggregate_delta_chr2_fpath(data_folder):
     return os.path.join(data_folder, "loco_chr2_aggregate_delta_output.xlsx")
 
 
+@pytest.fixture(scope="module")
+def loco_chr2_traces_fpath(data_folder):
+    return os.path.join(data_folder, "assembled_traces_ChR2.h5")
+
+
 def test_data_folder_exists(data_folder):
     assert os.path.exists(data_folder)
 
 
-def test_loco_chr2_files_exist(loco_chr2_fpath, loco_chr2_delta_fpath, loco_aggregate_chr2_fpath, loco_aggregate_delta_chr2_fpath):
+def test_loco_chr2_files_exist(loco_chr2_fpath, loco_chr2_delta_fpath, loco_aggregate_chr2_fpath, loco_aggregate_delta_chr2_fpath, loco_chr2_traces_fpath):
     assert os.path.exists(loco_chr2_fpath)
     assert os.path.exists(loco_chr2_delta_fpath)
     assert os.path.exists(loco_aggregate_chr2_fpath)
     assert os.path.exists(loco_aggregate_delta_chr2_fpath)
+    assert os.path.exists(loco_chr2_traces_fpath)
 
 
 @pytest.fixture(scope="module")
@@ -64,3 +73,18 @@ def loco_aggregate_chr2(loco_aggregate_chr2_fpath):
 @pytest.fixture(scope="module")
 def loco_aggregate_delta_chr2(loco_aggregate_delta_chr2_fpath):
     return pd.read_excel(loco_aggregate_delta_chr2_fpath)
+
+
+def test_locomotion_analysis_chr2_results(loco_chr2_df, loco_aggregate_chr2, loco_chr2_delta_df, loco_aggregate_delta_chr2, loco_chr2_traces_fpath):
+    dfs = main(fpath=loco_chr2_traces_fpath, save_data=False)
+    assert isinstance(dfs, tuple)
+    assert len(dfs) == 4
+    # TODO: make utility function to compare dataframes (with bools NaN stuff), use it to compare with should-be output
+    df, df_aggregate, df_delta, df_delta_aggregate = dfs
+    # TODO: need to implement approximate equality comparison for floats.
+    assert dataframes_equal(df, loco_chr2_df, both_nan_equal=True)
+    assert dataframes_equal(
+        df_aggregate, loco_aggregate_chr2, both_nan_equal=True)
+    assert dataframes_equal(df_delta, loco_chr2_delta_df, both_nan_equal=True)
+    assert dataframes_equal(
+        df_delta_aggregate, loco_aggregate_delta_chr2, both_nan_equal=True)
